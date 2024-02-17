7b4b1111d54387630ffc5320867b4bb729f45477
Sharkpb8 <sharkpb8@gmail.com>
2024-02-17 17:57:49 +0100

    first commit

---
+++ Database project/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while(true)
+        {
+            Console.WriteLine("1. Vložit do databáze");
+            Console.WriteLine("2. Smazat z databáze");
+            Console.WriteLine("3. Úprava záznamu");
+        }
+    }
+}
