88e17943d956790838aae93e9e1c3d3b324c6f47
Sharkpb8 <sharkpb8@gmail.com>
2024-02-17 18:26:51 +0100

    added more options

--- Database project/Program.cs
+++ Database project/Program.cs
@@ -27,10 +27,40 @@ internal class Program
             }
             else if(answer == "4")
             {
-                Console.WriteLine("List of autor");
-                foreach (Autor i in c.GetAll())
+                Console.WriteLine("Kterou tabulku chcete vypsat");
+                Console.WriteLine("1. autor");
+                Console.WriteLine("2. žánr");
+                Console.WriteLine("3. knihy");
+                Console.WriteLine("4. košík");
+                Console.WriteLine("5. zákazník");
+                answer = Console.ReadLine();
+                if (answer == "1")
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine("List of autor");
+                    foreach (Autor i in c.GetAll())
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                else if (answer == "2")
+                {
+
+                }
+                else if(answer == "3")
+                {
+
+                }
+                else if( answer == "4")
+                {
+
+                }
+                else if(answer== "5")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
                 }
             }
             else
