c9277da89fe61eaea1e483ca2e7d04d669f54153
Sharkpb8 <sharkpb8@gmail.com>
2024-06-02 14:00:18 +0200

    publisher added to book and bookdao

--- Database project/Book.cs
+++ Database project/Book.cs
@@ -11,6 +11,7 @@ namespace Database_project
         private int id;
         private int genre_id;
         private int autor_id;
+        private int publisher_id;
         private string name;
         private string release_date;
 
@@ -18,17 +19,19 @@ namespace Database_project
         public int ID { get => id; set => id = value; }
         public int Genre_id { get => genre_id; set => genre_id = value; }
         public int Autor_id { get => autor_id; set => autor_id = value; }
+        public int Publisher_id { get => publisher_id; set => publisher_id = value; }
         public string Name { get => name; set => name = value; }
         public string Release_date { get => release_date; set => release_date = value; }
 
         /// <summary>
         /// Konstruktor třídy book.
         /// </summary>
-        public Book(int id,int genre_id,int autor_id, string name, string release_date)
+        public Book(int id,int genre_id,int autor_id,int publisher_id, string name, string release_date)
         {
             this.ID = id;
             this.genre_id = genre_id;
             this.autor_id = autor_id;
+            this.publisher_id = publisher_id;
             this.name = name;
             this.release_date = release_date;
         }
@@ -36,11 +39,12 @@ namespace Database_project
         /// <summary>
         /// Konstruktor třídy book pro vytvoření instance s neznámým ID
         /// </summary>
-        public Book(int genre_id, int autor_id, string name, string release_date)
+        public Book(int genre_id, int autor_id,int publisher_id, string name, string release_date)
         {
             this.ID = 0;
             this.genre_id = genre_id;
             this.autor_id = autor_id;
+            this.publisher_id= publisher_id;
             this.name = name;
             this.release_date = release_date;
         }
@@ -51,7 +55,7 @@ namespace Database_project
         /// <returns>Textový řetězec obsahující informace o knize.</returns>
         public override string ToString()
         {
-            return $"{ID}. {genre_id} {autor_id} {name} {release_date}";
+            return $"{ID}. {genre_id} {autor_id} {publisher_id} {name} {release_date}";
         }
     }
 }
--- Database project/BookDAO.cs
+++ Database project/BookDAO.cs
@@ -25,8 +25,9 @@ namespace Database_project
                         Convert.ToInt32(reader[0].ToString()),
                         Convert.ToInt32(reader[1].ToString()),
                         Convert.ToInt32(reader[2].ToString()),
-                        reader[3].ToString(),
-                        reader[4].ToString()
+                        Convert.ToInt32(reader[3].ToString()),
+                        reader[4].ToString(),
+                        reader[5].ToString()
                     );
                     yield return book;
                 }
@@ -41,10 +42,11 @@ namespace Database_project
 
             SqlCommand command = null;
 
-            using (command = new SqlCommand("insert into book (genre_id,autor_id,name,release_date) values (@genreid, @autorid,@name,@releasedate)", conn))
+            using (command = new SqlCommand("insert into book (genre_id,autor_id,publisher_id,name,release_date) values (@genreid, @autorid,@publisherid,@name,@releasedate)", conn))
             {
                 command.Parameters.Add(new SqlParameter("@genreid", b.Genre_id));
                 command.Parameters.Add(new SqlParameter("@autorid", b.Autor_id));
+                command.Parameters.Add(new SqlParameter("@publisherid", b.Publisher_id));
                 command.Parameters.Add(new SqlParameter("@name", b.Name));
                 command.Parameters.Add(new SqlParameter("@releasedate", b.Release_date));
                 try
@@ -88,11 +90,12 @@ namespace Database_project
 
             SqlCommand command = null;
 
-            using (command = new SqlCommand("update book set genre_id = @gi, autor_id = @ai, name = @name, release_date = @rd " + "where id = @id", conn))
+            using (command = new SqlCommand("update book set genre_id = @gi, autor_id = @ai,publisher_id = @pi, name = @name, release_date = @rd " + "where id = @id", conn))
             {
                 command.Parameters.Add(new SqlParameter("@id", b.ID));
                 command.Parameters.Add(new SqlParameter("@gi", b.Genre_id));
                 command.Parameters.Add(new SqlParameter("@ai", b.Autor_id));
+                command.Parameters.Add(new SqlParameter("@pi", b.Publisher_id));
                 command.Parameters.Add(new SqlParameter("@name", b.Name));
                 command.Parameters.Add(new SqlParameter("@rd", b.Release_date));
                 try
@@ -119,10 +122,11 @@ namespace Database_project
                 {
                     int genre_id = int.Parse(bn.SelectSingleNode("genre_id").InnerText);
                     int autor_id = int.Parse(bn.SelectSingleNode("autor_id").InnerText);
+                    int publisher_id = int.Parse(bn.SelectSingleNode("publisher_id").InnerText);
                     string name = bn.SelectSingleNode("name").InnerText;
                     string release_date = bn.SelectSingleNode("release_date").InnerText;
 
-                    Book a = new Book(genre_id, autor_id, name, release_date);
+                    Book a = new Book(genre_id, autor_id,publisher_id, name, release_date);
                     Save(a);
                 }
             }
